d5ac75a11999de44aaf81a16cb9d6829d51cad4e
zalenscarpentier <168520191+zalenscarpentier@users.noreply.github.com>
2025-01-03 08:32:42 +0100

    Update contract deployment script

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -7,13 +7,13 @@ module.exports = async function(deployer) {
   
   // After deployment, you can create a farm with its token using createFarmWithToken
   await farmFactory.createFarmWithToken(
-    "My Farm", // _farmName
-    "Farm Token", // _tokenName
-    "FARM", // _tokenSymbol
-    "100", // _sizeInAcres
-    "1000000000000000000000000", // _totalTokenSupply (1 million tokens with 18 decimals)
-    "1000000000000000000000", // _valuation (1000 ETH in wei)
-    "10", // _expectedOutcomePercentage (10%)
-    "1000000000000000000" // _pricePerToken (1 ETH in wei)
+    "Ferme du Quennelet", // _farmName
+    "Quennelet Farm Token", // _tokenName
+    "QFT", // _tokenSymbol
+    "122", // _sizeInAcres
+    "100000", // _totalTokenSupply 
+    "1000000", // _valuation 
+    "10", // _expectedOutcomePercentage 
+    "1" // _pricePerToken
   );
 }; 
