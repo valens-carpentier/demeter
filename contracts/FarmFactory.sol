4a3ba41f27490270025e38bcc1c4b4a60683be05
valens-carpentier <valenscarpentier@gmail.com>
2025-01-21 15:48:27 +0100

    refactor: use mapping instead of array

--- contracts/FarmFactory.sol
+++ contracts/FarmFactory.sol
@@ -16,17 +16,25 @@ contract FarmFactory {
         uint256 timestamp;   // When the farm was created
     }
 
-    Farm[] public farms;
-
-    mapping(address => uint[]) public farmsByOwner;
+    // Replace farms array with mapping
+    mapping(bytes32 => Farm) public farms;
+    mapping(address => bytes32[]) public farmsByOwner;
     mapping(address => uint256) public ownerFarmCount;
+    
+    // Keep track of all farm IDs
+    bytes32[] public allFarmIds;
 
-    event FarmCreated(uint256 farmId, address owner, string name, uint256 timestamp);
-    event FarmDeactivated(uint256 farmId, uint256 timestamp);
+    event FarmCreated(bytes32 indexed farmId, address owner, string name, uint256 timestamp);
+    event FarmDeactivated(bytes32 indexed farmId, uint256 timestamp);
 
-    modifier onlyFarmOwner(uint256 _farmId) {
+    modifier onlyFarmOwner(bytes32 _farmId) {
         require(farms[_farmId].owner == msg.sender, "Not the farm owner");
         _;
+    }
+
+    // Helper function to generate farm ID
+    function generateFarmId(string memory _name) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(_name));
     }
 
     function createFarm(
@@ -36,12 +44,15 @@ contract FarmFactory {
         uint256 _totalTokenSupply,
         uint256 _valuation,
         uint256 _expectedOutcomePercentage
-    ) internal returns (uint256) {
+    ) internal returns (bytes32) {
         require(_token != address(0), "Invalid token address");
         require(bytes(_name).length > 0, "Name cannot be empty");
         require(_sizeInAcres > 0, "Size must be greater than 0");
         require(_totalTokenSupply > 0, "Total token supply must be greater than 0");
         
+        bytes32 farmId = generateFarmId(_name);
+        require(farms[farmId].owner == address(0), "Farm name already exists");
+
         Farm memory newFarm = Farm({
             token: _token,
             owner: msg.sender,
@@ -54,33 +65,32 @@ contract FarmFactory {
             timestamp: block.timestamp
         });
         
-        farms.push(newFarm);
-        uint256 newFarmId = farms.length - 1;
-        
-        farmsByOwner[msg.sender].push(newFarmId);
+        farms[farmId] = newFarm;
+        farmsByOwner[msg.sender].push(farmId);
+        allFarmIds.push(farmId);
         ownerFarmCount[msg.sender]++;
         
-        emit FarmCreated(newFarmId, msg.sender, _name, block.timestamp);
+        emit FarmCreated(farmId, msg.sender, _name, block.timestamp);
         
-        return newFarmId;
+        return farmId;
     }
     
-    function getFarm(uint256 _farmId) public view returns (Farm memory) {
-        require(_farmId < farms.length, "Farm does not exist");
+    function getFarm(bytes32 _farmId) public view returns (Farm memory) {
+        require(farms[_farmId].owner != address(0), "Farm does not exist");
         return farms[_farmId];
     }
     
-    function deactivateFarm(uint256 _farmId) public onlyFarmOwner(_farmId) {
+    function deactivateFarm(bytes32 _farmId) public onlyFarmOwner(_farmId) {
         require(farms[_farmId].isActive, "Farm is already inactive");
         farms[_farmId].isActive = false;
         emit FarmDeactivated(_farmId, block.timestamp);
     }
     
     function getTotalFarms() public view returns (uint256) {
-        return farms.length;
+        return allFarmIds.length;
     }
     
-    function updateFarmValuation(uint256 _farmId, uint256 _newValuation) public onlyFarmOwner(_farmId) {
+    function updateFarmValuation(bytes32 _farmId, uint256 _newValuation) public onlyFarmOwner(_farmId) {
         require(farms[_farmId].isActive, "Farm is inactive");
         farms[_farmId].valuation = _newValuation;
     }
@@ -94,19 +104,19 @@ contract FarmFactory {
         uint256 _valuation,
         uint256 _expectedOutcomePercentage,
         uint256 _pricePerToken
-    ) public returns (uint256) {
+    ) public returns (bytes32) {
         // Create new farm token
         FarmToken newToken = new FarmToken(
             _tokenName,
             _tokenSymbol,
             _totalTokenSupply,
-            farms.length, // This will be the new farm's ID
+            allFarmIds.length, // This will be the new farm's ID
             address(this),
             _pricePerToken
         );
         
         // Create farm using existing createFarm function
-        uint256 farmId = createFarm(
+        bytes32 farmId = createFarm(
             address(newToken),
             _farmName,
             _sizeInAcres,
@@ -118,8 +128,8 @@ contract FarmFactory {
         return farmId;
     }
     
-    function getFarmTokenAddress(uint256 _farmId) public view returns (address) {
-        require(_farmId < farms.length, "Farm does not exist");
+    function getFarmTokenAddress(bytes32 _farmId) public view returns (address) {
+        require(farms[_farmId].owner != address(0), "Farm does not exist");
         return farms[_farmId].token;
     }
 }
